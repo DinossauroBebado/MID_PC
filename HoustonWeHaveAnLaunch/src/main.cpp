b6b999d364a60bbd813c64184a524b7341901ae2
DinossauroBebado <guipsilva32@gmail.com>
2021-12-27 00:55:50 -0300

    for now finished

--- HoustonWeHaveAnLaunch/src/main.cpp
+++ HoustonWeHaveAnLaunch/src/main.cpp
@@ -1,4 +1,10 @@
-
+//code by : DinossauroBebado
+// MID controls for Windows PC
+//references :
+//https://github.com/RalphBacon/184-USB-Volume-Control
+//https://github.com/NicoHood/HID
+//obs : this code could be refactor to be way shorter by using functions,
+// but o wrote like that to be easily readable
 
 #include <RotaryEncoder.h>
 #include <HID-Project.h>
@@ -7,10 +13,13 @@
 #define A 6
 #define B 4
 #define muteAudio 15
-#define muteMic 3
-#define setPlaylist 9
+#define muteMic 9
+#define setPlaylist 3
 #define playMusic 10
 #define setAudioOut 16
+// Comment out next line to prevent slow response when
+// not actively debugging via serial port monitor
+#define DEBUG
 
 RotaryEncoder encoder(A, B);
 
@@ -34,10 +43,12 @@ void setup()
   pinMode(setPlaylist, INPUT);
   pinMode(playMusic, INPUT);
   pinMode(setAudioOut, INPUT);
-  prev_micMuted = digitalRead(muteAudio);
+#ifdef DEBUG
   Serial.begin(9600);
+#endif
 
   Consumer.begin();
+  Keyboard.begin();
 }
 
 void loop()
@@ -47,70 +58,152 @@ void loop()
   audioMuted = digitalRead(muteAudio);
   if (audioMuted != 1)
   {
+#ifdef DEBUG
     Serial.println("Muda estado do volume geral");
+#endif
+
     Consumer.write(MEDIA_VOLUME_MUTE);
     while (digitalRead(muteAudio) == 0)
       delay(10);
   }
 
-  //muta o microfone--------------------
+  //MICROFONE--------------------
   micMuted = digitalRead(muteMic);
   if (micMuted == 0 && prev_micMuted == 1)
-  {
+  { //mute mic--------------------
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_RIGHT_CTRL);
+    Keyboard.press('4');
+    delay(100);
+    Keyboard.releaseAll();
+
+#ifdef DEBUG
     Serial.println("Microfone desmutado");
+#endif
+
     prev_micMuted = 0;
+
     delay(10);
   }
 
   if (micMuted == 1 && prev_micMuted == 0)
-  {
+  { //unmute m--------------------
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_RIGHT_CTRL);
+    Keyboard.press('3');
+    delay(100);
+    Keyboard.releaseAll();
+
+#ifdef DEBUG
     Serial.println("Microfone mutado");
+#endif
+
     prev_micMuted = 1;
-    delay(10);
+
+    delay(100);
   }
 
-  // play spotify playlist----------------
+  //spotify playlist----------------
   musicPlaying = digitalRead(playMusic);
   if (musicPlaying == 1 && prev_musicPlaying == 0)
-  {
+  { // play spotify playlist----------------
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_RIGHT_CTRL);
+    Keyboard.press('5');
+    delay(100);
+    Keyboard.releaseAll();
+
+#ifdef DEBUG
     Serial.println("Play music");
+#endif
+
     prev_musicPlaying = 1;
-    delay(10);
+    delay(100);
   }
   if (musicPlaying == 0 && prev_musicPlaying == 1)
-  {
+  { //pause spotify playlist----------------
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_RIGHT_CTRL);
+    Keyboard.press('6');
+    delay(100);
+    Keyboard.releaseAll();
+
+#ifdef DEBUG
     Serial.println("Stop music");
+#endif
+
     prev_musicPlaying = 0;
-    delay(10);
+
+    delay(100);
   }
   // choose spotify playlist-----------------
   lofi = digitalRead(setPlaylist);
   if (lofi == 1 && prev_lofi == 0)
-  {
+  { // choose lofi playlist-----------------
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_RIGHT_CTRL);
+    Keyboard.press('1');
+    delay(100);
+    Keyboard.releaseAll();
+
+#ifdef DEBUG
     Serial.println("Playlist: LOFI");
+#endif
+
     prev_lofi = 1;
-    delay(10);
+
+    delay(100);
   }
   if (lofi == 0 && prev_lofi == 1)
-  {
+  { // choose rock  playlist-----------------
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_RIGHT_CTRL);
+    Keyboard.press('2');
+    delay(100);
+    Keyboard.releaseAll();
+
+#ifdef DEBUG
     Serial.println("Playlist: ROCK");
+#endif
+
     prev_lofi = 0;
-    delay(10);
+
+    delay(100);
   }
 
   // choose audio OUTPUT----------------------
   audioFone = digitalRead(setAudioOut);
   if (audioFone == 1 && prev_audioFone == 0)
-  {
+  { // choose fone output -----------------
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_RIGHT_CTRL);
+    Keyboard.press('7');
+    delay(100);
+    Keyboard.releaseAll();
+
+#ifdef DEBUG
     Serial.println("Output:Fone");
+#endif
+
     prev_audioFone = 1;
-    delay(10);
+
+    delay(100);
   }
   if (audioFone == 0 && prev_audioFone == 1)
-  {
-    Serial.println("Output:Tv");
+  { // choose fone output -----------------
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_RIGHT_CTRL);
+    Keyboard.press('8');
+    delay(100);
+    Keyboard.releaseAll();
+
+#ifdef DEBUG
+    Serial.println("Output:TV");
+#endif
+
     prev_audioFone = 0;
-    delay(10);
+
+    delay(100);
   }
 
   //Le as informacoes do encoder
@@ -122,13 +215,19 @@ void loop()
   {
     if (pos > newPos)
     { //aumenta o volume
+#ifdef DEBUG
       Serial.print(newPos);
       Serial.println(" MAIS");
+#endif
+
       Consumer.write(MEDIA_VOL_DOWN);
     }
     if (pos < newPos)
     { //diminui o volume
+#ifdef DEBUG
       Serial.print(newPos);
+#endif
+
       Consumer.write(MEDIA_VOL_UP);
     }
     pos = newPos;
