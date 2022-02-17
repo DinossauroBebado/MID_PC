f6806837081a6262c2f6da4967fcec55072490ab
DinossauroBebado <guipsilva32@gmail.com>
2022-02-17 19:32:14 -0300

    simple refactor

--- py/init.py
+++ py/init.py
@@ -1,4 +1,4 @@
-from main import main
+from keys_listener import main
 import web_config
 import threading
 from bottle import run
@@ -6,5 +6,4 @@ from bottle import run
 if __name__ == "__main__":
     threading.Thread(target=run, kwargs=dict(
         host='localhost', port=8080)).start()
-    y = threading.Thread(target=main())
-    y.start()
+    y = threading.Thread(target=main()).start()
--- py/web_config.py
+++ py/web_config.py
@@ -1,6 +1,6 @@
 
 
-from bottle import run, route, request
+from bottle import route, request
 from config import read_config, write_config
 
 
@@ -9,11 +9,45 @@ def index():
     lofi, rock = read_config()
 
     return f"""
-        <form  action = "/" method = "post" >
-        LOFI : <input name = "LOFI" type = "text" value= {lofi}>
-        ROCK : <input name = "ROCK" type = "text"  value={rock}>
-        <input value = "SAVE" type = "submit"/>
-        </form>"""
+        <style>
+    input[type=text],
+    select {{
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }}
+
+    input[type=submit] {{
+        width: 100%;
+        background-color: #4CAF50;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }}
+
+    input[type=submit]:hover {{
+        background-color: #45a049;
+    }}
+
+    div {{
+        border-radius: 5px;
+        background-color: #f2f2f2;
+        padding: 20px;
+    }}
+</style>
+
+<form action="/" method="post">
+    LOFI : <input name="LOFI" type="text" value={lofi}>
+    ROCK : <input name="ROCK" type="text" value={rock}>
+    <input value="SAVE" type="submit" />
+</form>"""
 
 
 @route('/', method="POST")
@@ -24,11 +58,42 @@ def index():
     print(lofi, rock)
 
     return f"""
-        <form  action = "/" method = "post" >
-        LOFI : <input name = "LOFI" type = "text" value= "{lofi}"/>
-        ROCK : <input name = "ROCK" type = "text"  value="{rock}" />
-        <input value = "SAVE" type = "submit"/>
-        </form>"""
+            <style>
+        input[type=text],
+        select {{
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }}
 
+        input[type=submit] {{
+            width: 100%;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }}
 
-# run(port=8080)
+        input[type=submit]:hover {{
+            background-color: #45a049;
+        }}
+
+        div {{
+            border-radius: 5px;
+            background-color: #f2f2f2;
+            padding: 20px;
+        }}
+    </style>
+
+    <form action="/" method="post">
+        LOFI : <input name="LOFI" type="text" value={lofi}>
+        ROCK : <input name="ROCK" type="text" value={rock}>
+        <input value="SAVE" type="submit" />
+    </form>"""
