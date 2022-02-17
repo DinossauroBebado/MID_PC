11c0ae2ca9cf90c9f2652dc2ff55dbb54b0b1596
DinossauroBebado <guipsilva32@gmail.com>
2022-02-17 19:48:24 -0300

    add coments

--- py/init.py
+++ py/init.py
@@ -3,6 +3,8 @@ import web_config
 import threading
 from bottle import run
 
+# start code here -> runs the keys listener and the web config page
+
 if __name__ == "__main__":
     threading.Thread(target=run, kwargs=dict(
         host='localhost', port=8080)).start()
